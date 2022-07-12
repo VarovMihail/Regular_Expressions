fde5051ac38edd067a5b8595f058c26e0463a8b3
Варов Михаил <8605495@mail.ru>
2022-07-12 21:39:31 +0300

    Add task 3

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-# Структура данных будет всегда:
-# lastname,firstname,surname,organization,position,phone,email
-# Предполагается, что телефон и e-mail у человека может быть только один.
-# Необходимо:
-# поместить Фамилию, Имя и Отчество человека в поля lastname, firstname и surname соответственно.
-# В записной книжке изначально может быть Ф + ИО, ФИО, а может быть сразу правильно: Ф+И+О;
-# привести все телефоны в формат +7(999)999-99-99. Если есть добавочный номер, формат будет такой: +7(999)999-99-99 доб.9999;
-# объединить все дублирующиеся записи о человеке в одну.
+
 import re
 from pprint import pprint
 # читаем адресную книгу в формате CSV в список contacts_list
@@ -13,25 +6,24 @@ import csv
 with open("phonebook_raw.csv", encoding='UTF-8') as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
-#pprint(contacts_list)
+pprint(contacts_list)
+
 
 # TODO 1: выполните пункты 1-3 ДЗ
 # 1 поместить Фамилию, Имя и Отчество человека в поля lastname, firstname и surname
-l = []
-for i in contacts_list:
-  k = []
-  for id, text in enumerate(i):
-    if id in (0,1,2) and len(text.split()) > 1:
-      k.extend(text.split())
-    else:
-      k.append(text)
-  l.append(k)
-pprint(l)
-print('_____________________________________________')
+
+right_list = []
+for in_list in contacts_list:
+  fio_list = ' '.join(in_list[:3]).split()
+  while len(fio_list) < 3:
+    fio_list.append('')
+  new_in_list = fio_list + in_list[3:]
+  right_list.append(new_in_list)
 
 # 2 привести все телефоны в формат +7(999)999-99-99. Если есть добавочный номер, формат будет такой: +7(999)999-99-99 доб.9999;
+
 l1 = []
-for i in l:
+for i in right_list:
   k1 = []
   for text in i:
     pattern = r'(\+7|8)?\s*\(?(\d{3})\)?[\s-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})\s*\(?([доб.]*\s*\d*)\)?'
@@ -43,14 +35,29 @@ for i in l:
 pprint(l1)
 
 # 3 объединить все дублирующиеся записи о человеке в одну.
+d = {}
+for in_list in l1:
+  if ' '.join(in_list[:2]) not in d.keys():
+    d[' '.join(in_list[:2])] = in_list[2:]
+  else:
+    l = []
+    for id, elem in enumerate(d[' '.join(in_list[:2])]):
+      if elem:
+        l.append(elem)
+      else:
+        l.append(in_list[2:][id])
+    d[' '.join(in_list[:2])] = l
+pprint(d)
 
+new_right_list = []
+for key, val in d.items():
+  new_right_list.append(key.split() + val)
 
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
 with open("phonebook.csv", "w", encoding='UTF-8') as f:
-  datawriter = csv.writer(f, delimiter=',')
-  # Вместо contacts_list подставьте свой список
-  datawriter.writerows(l1)
+  datawriter = csv.writer(f, delimiter=',',lineterminator = '\n')
+  datawriter.writerows(new_right_list)
 
 
 
